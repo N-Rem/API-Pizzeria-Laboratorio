2d6e98eb3eb96aaf9ea01231b182c309ac526af7
N-Rem <nicolasromero.barrios@gmail.com>
2024-06-26 14:38:32 -0300

    Add Entities

--- API-Pizzeria/API_Pizzeria/Domain/Entities/User.cs
+++ API-Pizzeria/API_Pizzeria/Domain/Entities/User.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,25 @@ namespace Domain.Entities
 {
     public class User
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public UserRol rol {  get; set; }
+
+        public ICollection<UserProduct> UserProducts { get; set; } = new List<UserProduct>();
+
+        public enum UserRol
+        {
+            Client,
+            Admin,
+            SuperAdmin,
+        }
+
+
     }
 }
---
+++ API-Pizzeria/API_Pizzeria/Domain/Entities/UserProduct.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class UserProduct
+    {
+        public int Id {  get; set; }
+        public int UserId {  get; set; }
+        public User User { get; set; }
+
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
